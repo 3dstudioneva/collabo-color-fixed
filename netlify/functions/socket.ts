20d7c3e31ceb054f02be89a75a629008dad738ce
3dstudioneva <golyakin95@yandex.ru>
2025-11-21 19:12:59 +0300

    fix: correct netlify function handler

--- netlify/functions/socket.ts
+++ netlify/functions/socket.ts
@@ -1,11 +1,7 @@
-import express from 'express';
-import http from 'http';
 import { Server } from 'socket.io';
-import serverless from 'serverless-http';
+import type { Handler } from '@netlify/functions';
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server();
 
 io.on('connection', (socket) => {
   console.log('a user connected');
@@ -35,8 +31,12 @@ io.on('connection', (socket) => {
   });
 });
 
-app.get('/', (req, res) => {
-  res.send('Socket.io server is running.');
-});
+const handler: Handler = async (event, context) => {
+  // @ts-ignore
+  io.listen(3001);
+  return {
+    statusCode: 200,
+  };
+};
 
-export const handler = serverless(app);
+export { handler };
